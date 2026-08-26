e884284e0bbc0383521f0e4ef8561f6d62301204
rakuhiyoshi-commits <raku.hiyoshi@gmail.com>
2026-08-26 09:55:43 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 // ここだけ書き換えれば表示内容を更新できます。
 // 金額は田中貴金属 RE:TANAKA リサイクル価格を確認して、手入力してください。
 window.SIGNAGE_DATA = {
-  slideSeconds: 10,
+  slideSeconds: 6,
   shopName: "あーるさいず",
   place: "YOKOHAMA HIYOSHI",
 
